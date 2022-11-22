3eb3f1abef37f4534812081cf167be4cbd521843
majid izadi <izadi.majid73@gmail.com>
2022-11-22 10:58:36 +0330

    add menu toggle style with javascript

---
+++ script.js
@@ -0,0 +1,17 @@
+// menu toggle
+let toggle = document.querySelector('.toggle');
+let navigation = document.querySelector('.navigation');
+let main = document.querySelector('.main');
+
+toggle.onclick = function(){
+   navigation.classList.toggle('active');
+   main.classList.toggle('active');
+}
+// add hoverd class in select list item
+  let list = document.querySelectorAll('.navigation li');
+  function activelink(){
+      list.forEach((item)=>item.classList.remove('hovered'));
+      this.classList.add('hovered');
+  }
+  list.forEach((item)=>
+  item.addEventListener('mouseover',activelink));
